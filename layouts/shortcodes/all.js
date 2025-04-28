47c394c3c5bc3d0df2c45978a055f46d9e67c57a
theexiile1305 <development@michifuchs.com>
2025-04-28 12:10:46 +0200

    feat: remove youtube

--- layouts/shortcodes/all.js
+++ layouts/shortcodes/all.js
@@ -6,7 +6,6 @@ import Notice from "./Notice";
 import Tab from "./Tab";
 import Tabs from "./Tabs";
 import Video from "./Video";
-import Youtube from "./Youtube";
 
 const shortcodes = {
   Button,
@@ -16,7 +15,6 @@ const shortcodes = {
   Tabs,
   Notice,
   Code,
-  Youtube,
   Blockquote,
 };
 
